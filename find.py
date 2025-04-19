01ab079c5e329d3251f52b6f00985f80dac0d819
Plice13 <pliceduard@gmail.com>
2025-04-20 01:13:36 +0200

    fix obrazky recniku

--- find.py
+++ find.py
@@ -46,7 +46,7 @@ def find_string_in_files(directory_path, search_string):
 
 if __name__ == "__main__":
     # Define the string to search for
-    string_to_find = "graphic"
+    string_to_find = "speaker-photo"
 
 
     # Get the directory where the script is located
