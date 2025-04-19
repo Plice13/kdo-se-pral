9146c6350f5337272beb757aded7147f957e16a9
Plice13 <pliceduard@gmail.com>
2025-04-20 00:18:32 +0200

    dasd

--- find.py
+++ find.py
@@ -46,7 +46,7 @@ def find_string_in_files(directory_path, search_string):
 
 if __name__ == "__main__":
     # Define the string to search for
-    string_to_find = "Pomáháme"
+    string_to_find = "highlight-primary"
 
     # Get the directory where the script is located
     # This will be the starting point for os.walk()
