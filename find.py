0f51ccabda24c2a40d82f0012d830b8ffc0bc5a2
Plice13 <pliceduard@gmail.com>
2025-04-20 13:23:07 +0200

    revirt to functionl without darkmode

--- find.py
+++ find.py
@@ -46,7 +46,7 @@ def find_string_in_files(directory_path, search_string):
 
 if __name__ == "__main__":
     # Define the string to search for
-    string_to_find = "prefers-color-scheme"
+    string_to_find = "darkomode"
 
 
     # Get the directory where the script is located
